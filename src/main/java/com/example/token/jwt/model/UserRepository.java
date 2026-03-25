1abd1a57665eb36543479fe5c0d6d7cfa5565f43
girish9046b <90566230+girish9046b@users.noreply.github.com>
2026-03-25 15:00:25 +0530

    Final code to generate token and register new user

--- src/main/java/com/example/token/jwt/model/UserRepository.java
+++ src/main/java/com/example/token/jwt/model/UserRepository.java
@@ -4,7 +4,7 @@ package com.example.token.jwt.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, String> {
 public User findByUserNameAndPassword(String username, String password);
 public User findByUserName(String username);
 }
